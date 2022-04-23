128f9d6a83846ffc66f0c82b998c9b85dc921856
Ahmed Kira <50806393+ahm3dkira@users.noreply.github.com>
2022-04-23 20:30:44 +0200

    Update settings.py

--- Simpleskill/settings.py
+++ Simpleskill/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,172.17.0.1,simpleskill.test.ahmedkira.com,simpleskill.nourgaser.com").split(",")
+
 
 
 # Application definition
