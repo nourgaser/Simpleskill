7fe2a4b23acf05a568998fd02ffae5e2ee247fa6
nour <nourgaser2012@gmail.com>
2022-05-27 19:00:51 +0200

    added footer

--- Simpleskill/urls.py
+++ Simpleskill/urls.py
@@ -20,8 +20,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("app.urls")),
     path('app/', include("app.urls")),
-    path('', include("website.urls")),
+    # path('', include("website.urls")),
     path('', include('django.contrib.auth.urls')),
     path('', include('accounts.urls')),
 ]
